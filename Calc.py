71787a2d0b8b71feba9629cd06620cf0f44bafd1
devesh18-design <devesh.25bai10381@vitbhopal.ac.in>
2025-11-20 09:34:01 +0530

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,69 @@
+from tkinter import*
+
+
+def bttnClick(numbers):
+    global operator
+    operator = operator + str(numbers)
+    text_input.set(operator)
+
+def bttnClear():
+    global operator
+    operator = ""
+    text_input.set("")
+
+def bttnEquals():
+    global operator
+    sumup = str(eval(operator))
+    text_input.set(sumup)
+    operator = ""
+
+calc = Tk()
+calc.title("Calculator by Devesh")
+operator = ""
+text_input = StringVar()
+
+textDisplay = Entry(calc, font = ('arial', 20, 'bold'), textvariable = text_input, bd = 30, insertwidth = 4, bg = 'white', justify = 'right').grid(columnspan = 4)
+
+
+
+button_7 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'black', font = ('arial', 20, 'bold'), text = '7', bg = 'white', command = lambda:bttnClick(7)).grid(row = 1, column = 0)
+
+button_8 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'black', font = ('arial', 20, 'bold'), text = '8', bg = 'white', command = lambda:bttnClick(8)).grid(row = 1, column = 1)
+
+button_9 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'black', font = ('arial', 20, 'bold'), text = '9', bg = 'white', command = lambda:bttnClick(9)).grid(row = 1, column = 2)
+
+Addition_button = Button(calc, padx = 16,pady = 16, bd = 8, fg = 'black', font = ('arial', 20, 'bold'), text = '+', bg = 'white', command = lambda:bttnClick('+')).grid(row = 1, column = 3)
+
+
+
+button_4 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'white', font = ('arial', 20, 'bold'), text = '4', bg = 'black', command = lambda:bttnClick(4)).grid(row = 2, column = 0)
+
+button_5 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'white', font = ('arial', 20, 'bold'), text = '5', bg = 'black', command = lambda:bttnClick(5)).grid(row = 2, column = 1)
+
+button_6 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'white', font = ('arial', 20, 'bold'), text = '6', bg = 'black', command = lambda:bttnClick(6)).grid(row = 2, column = 2)
+
+Subtraction_button = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'white', font = ('arial', 20, 'bold'), text = '-', bg = 'black', command = lambda:bttnClick('-')).grid(row = 2, column = 3)
+
+
+
+button_3 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'black', font = ('arial', 20, 'bold'), text = '3', bg = 'white', command = lambda:bttnClick(3)).grid(row = 3, column = 0)
+
+button_2 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'black', font = ('arial', 20, 'bold'), text = '2', bg = 'white', command = lambda:bttnClick(2)).grid(row = 3, column = 1)
+
+button_1 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'black', font = ('arial', 20, 'bold'), text = '1', bg = 'white', command = lambda:bttnClick(1)).grid(row = 3, column = 2)
+
+Multiplication_button = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'black', font = ('arial', 20, 'bold'), text = '*', bg = 'white', command = lambda:bttnClick('*')).grid(row = 3, column = 3)
+
+
+
+
+button_0 = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'white', font = ('arial', 20, 'bold'), text = '0', bg = 'black', command = lambda:bttnClick(0)).grid(row = 4, column = 0)
+
+button_Clear = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'white', font = ('arial', 20, 'bold'), text = 'Clr', bg = 'black', command = bttnClear).grid(row = 4, column = 1)
+
+button_Equals = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'white', font = ('arial', 20, 'bold'), text = '=', bg = 'gray', command = bttnEquals).grid(row = 4, column = 2)
+
+Division_button = Button(calc, padx = 16, pady = 16, bd = 8, fg = 'white', font = ('arial', 20, 'bold'), text = '/', bg = 'black', command = lambda:bttnClick('/')).grid(row = 4, column = 3)
+
+
+calc.mainloop()
